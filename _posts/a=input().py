8d372f5cb097e6db2075b7b182de250a8aea6d0f
StarRingCorporation <3688921976@qq.com>
2025-09-20 22:53:06 +0800

    Add files via upload

---
+++ _posts/a=input().py
@@ -0,0 +1,10 @@
+a=input()
+if a == '1':
+    print("这个是if的执行")
+elif a == '2':
+    print("这个是elif的执行")
+    print("似乎我没有看到if语句应该输出的内容啊")
+    print("似乎后面的东西我看不到啊")
+else:
+    print("前面两个都没有执行")
+    print("这个是else的执行")
